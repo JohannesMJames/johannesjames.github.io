0faa40c1a63f7d3eda78c15ecf789379736bffc4
Calvinosaurus <jmjames@uw.edu>
2024-09-23 22:14:13 -0700

    tweaked favicon

--- _make_favicon_windows.sh
+++ _make_favicon_windows.sh
@@ -1,1 +1,1 @@
-"C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\magick.exe" -background white "images/tank.png" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
+"C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\magick.exe" -background transparent "images/tank.png" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
